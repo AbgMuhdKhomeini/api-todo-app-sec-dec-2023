620bbd47086cd61a4507764ae60fb2cebb8e85fd
AbgMuhdKhomeini <amkbas89@gmail.com>
2024-02-01 21:56:08 +0800

    fix env

--- database/connection.js
+++ database/connection.js
@@ -4,16 +4,16 @@ const { Pool } = pkg;
 import "dotenv/config";
 
 const pool = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  port: process.env.PORT,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
   //add this for production
-  ssl: process.env.SSL,
+  ssl: process.env.DB_SSL,
 });
 // pool is a passport to send request to database
 
